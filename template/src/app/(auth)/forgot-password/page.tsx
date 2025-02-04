4371ae613517c1c700ec823ef16f9fd9bfaa00bf
Adel2411 <hadjarabadel.2411@gmail.com>
2025-02-04 04:04:11 +0100

    little change in the title of auth pages

--- template/src/app/(auth)/forgot-password/page.tsx
+++ template/src/app/(auth)/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import ForgotPasswordForm from "@/features/auth/components/ForgotPasswordForm";
 function ForgotPasswordPage() {
   return (
     <div className="flex flex-col items-center gap-10">
-      <h1 className="text-3xl md:text-4xl font-semibold">Forgot Password</h1>
+      <h1 className="text-3xl font-semibold">Forgot Password</h1>
       <ForgotPasswordForm />
     </div>
   );
--- template/src/app/(auth)/login/page.tsx
+++ template/src/app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/features/auth/components/LoginForm";
 function LoginPage() {
   return (
     <div className="flex flex-col items-center gap-10">
-      <h1 className="text-3xl md:text-4xl font-semibold">Login</h1>
+      <h1 className="text-3xl font-semibold">Login</h1>
       <LoginForm />
     </div>
   );
