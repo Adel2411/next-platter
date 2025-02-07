43c27f99d7aec5c164abdb3802f742842246678f
Adel2411 <hadjarabadel.2411@gmail.com>
2025-02-07 03:53:51 +0100

    Deleted ESLint and wrapped ResetPasswordForm with Suspense because it uses useSearchParams

--- template/src/app/(auth)/reset-password/page.tsx
+++ template/src/app/(auth)/reset-password/page.tsx
@@ -1,10 +1,13 @@
 import ResetPasswordForm from "@/features/auth/components/ResetPasswordForm";
+import { Suspense } from "react";
 
 function ResetPasswordPage() {
   return (
     <div className="flex flex-col items-center gap-10">
       <h1 className="text-3xl font-semibold">Reset Password</h1>
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 }
