a124b4f0003fadc8443aca37cb76b1fb75a0ca2f
Adel2411 <hadjarabadel.2411@gmail.com>
2025-02-08 16:07:27 +0100

    Refactored "/" and "/protected" pages by removing there features folder

--- template/src/app/page.tsx
+++ template/src/app/page.tsx
@@ -1,9 +1,39 @@
-import Main from "@/features/home/components/Main";
+import FeatureCard from "@/components/FeatureCard";
+import { Button } from "@/components/ui/button";
+import { features } from "@/constants";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className="flex justify-center pt-20 max-w-screen-lg">
-      <Main />
+      <div>
+        <div className="text-center">
+          <h1 className="mb-4 text-2xl font-bold sm:text-3xl md:text-4xl">
+            Front Starter Template
+          </h1>
+          <p className="mx-auto mb-6 max-w-lg text-muted-foreground">
+            A production-ready template for scalable, modular apps. Packed with
+            features like authentication, state management, and a rich ecosystem
+            of tools for modern development.
+          </p>
+          <div>
+            <Link href="/login" passHref>
+              <Button>
+                Start Building
+                <LogIn className="h-3 w-3" />
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="px-4 py-32 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {features.map((feature) => (
+            <div key={feature.id}>
+              <FeatureCard feature={feature} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- template/src/app/protected/page.tsx
+++ template/src/app/protected/page.tsx
@@ -1,9 +1,50 @@
-import Main from "@/features/protected/components/Main";
+"use client";
+
+import FeatureCard from "@/components/FeatureCard";
+import { Button } from "@/components/ui/button";
+import { features } from "@/constants";
+import { deleteToken } from "@/features/auth/api";
+import { showSuccessToast } from "@/lib/toastHandler";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function ProtectedPage() {
+  const router = useRouter();
+
+  const handleLogOut = async () => {
+    await deleteToken();
+
+    // On success
+    showSuccessToast("Logout successful");
+    router.push("/");
+  };
+
   return (
     <div className="flex justify-center pt-20 max-w-screen-lg">
-      <Main />
+      <div>
+        <div className="text-center">
+          <h1 className="mb-4 text-2xl font-bold sm:text-3xl md:text-4xl">
+            Protected Route
+          </h1>
+          <p className="mx-auto mb-6 max-w-lg text-muted-foreground">
+            You've successfully accessed a secure route. Start building your
+            protected features here.
+          </p>
+          <div>
+            <Button variant="destructive" onClick={handleLogOut}>
+              Logout
+              <LogOut className="h-3 w-3" />
+            </Button>
+          </div>
+        </div>
+        <div className="px-4 py-32 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+          {features.map((feature) => (
+            <div key={feature.id}>
+              <FeatureCard feature={feature} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
